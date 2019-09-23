0191331b1acedb29b0f082833159e7de88683c36
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2019-09-23 02:03:21 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,28 +3,31 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(array)
-  new_array = []
-  array.each do |element|
-    new_array.push("Hello, my name is " + element + ".")
+  #new_array = []
+  #array.each do |element|
+    #new_array.push("Hello, my name is " + element + ".")
+  #end
+  #new_array
+  array.collect do |element|
+    #"Hello, my name is " + element + "."
+    badge_maker(element)
   end
-  new_array
 end
 
 def assign_rooms(speakers)
-  new_array = []
-  room = 1
-  speakers.each do |speaker|
-    new_array.push("Hello, #{speaker}! You'll be assigned to room #{room}!")
-    room +=1
+  #new_array = []
+  #room = 1
+  #speakers.each do |speaker|
+    #new_array.push("Hello, #{speaker}! You'll be assigned to room #{room}!")
+    #room +=1
+  #end
+  #new_array
+  speakers.collect.with_index(1) do |speaker, index|
+    "Hello, #{speaker}! You'll be assigned to room #{index}!"
   end
-  new_array
 end
 
 def printer(attendees)
-  batch_badge_creator(attendees).each do |badge|
-    puts badge
-  end
-  assign_rooms(attendees).each do |rooms|
-    puts rooms
-  end
+  batch_badge_creator(attendees).each {|badge| puts badge}
+  assign_rooms(attendees).each {|rooms| puts rooms}
 end
